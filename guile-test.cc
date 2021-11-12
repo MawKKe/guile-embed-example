13b9c2d3ba5eb953f4fb18cdcfc40be722404f5f
Markus Holmström (MawKKe) <markus@mawkke.fi>
2021-11-12 04:58:31 +0200

    whitespace fix

--- guile-test.cc
+++ guile-test.cc
@@ -25,28 +25,27 @@ my_gettime(void)
 }
 
 static SCM
-my_hostname (void)
+my_hostname(void)
 {
-    char *s = getenv ("HOSTNAME");
+    char *s = getenv("HOSTNAME");
     if (s == NULL)
         scm_misc_error(__func__, "HOSTNAME env variable is null ~A", SCM_BOOL_F);
     else
-        return scm_from_locale_string (s);
+        return scm_from_locale_string(s);
 }
 
 static void
-inner_main (void *data, int argc, char **argv)
+inner_main(void *data, int argc, char **argv)
 {
     (void)data;
-    scm_c_define_gsubr ("my-hostname", 0, 0, 0, reinterpret_cast<scm_t_subr>(my_hostname));
-    scm_c_define_gsubr ("my-gettime", 0, 0, 0, reinterpret_cast<scm_t_subr>(my_gettime));
-    scm_c_define_gsubr ("my-pythagoras", 2, 0, 0, reinterpret_cast<scm_t_subr>(my_pythagoras));
-    scm_shell (argc, argv);
+    scm_c_define_gsubr("my-hostname", 0, 0, 0, reinterpret_cast<scm_t_subr>(my_hostname));
+    scm_c_define_gsubr("my-gettime", 0, 0, 0, reinterpret_cast<scm_t_subr>(my_gettime));
+    scm_c_define_gsubr("my-pythagoras", 2, 0, 0, reinterpret_cast<scm_t_subr>(my_pythagoras));
+    scm_shell(argc, argv);
 }
 
-    int
-main (int argc, char **argv)
+int main(int argc, char **argv)
 {
-    scm_boot_guile (argc, argv, inner_main, 0);
+    scm_boot_guile(argc, argv, inner_main, 0);
     return 0; /* never reached */
 }
